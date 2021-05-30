22cd591fe1b0cc01bb58655f10e1b65ffccd672d
trishalou <trishaloudb@gmail.com>
2021-05-29 22:16:50 -0400

    init

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="barebones_sso.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(barebones_sso.Startup))]
+
+namespace barebones_sso
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+    }
+}
